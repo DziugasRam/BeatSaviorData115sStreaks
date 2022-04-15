940d787a121bd2777f3c327837ac8fcc00561ad6
Dziugas Ramonas <55551604+DziugasRam@users.noreply.github.com>
2022-04-15 16:37:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+var dir = process.env.APPDATA + "/Beat Savior Data/";
+var result = [];
+fs.readdirSync(dir).forEach((fileName) => {
+    if (fileName[0] === "_") return;
+
+    fileData = fs.readFileSync(dir + fileName, { encoding: "utf8" });
+    fileData.split(/\r?\n/).forEach((line) => {
+        try {
+            var obj = JSON.parse(line);
+            var noteScores = obj.deepTrackers?.noteTracker?.notes?.map((t) => t.score[0] + t.score[1] + t.score[2]);
+            if (!noteScores) return;
+            var count = 0;
+            var maxCount = 0;
+            for (var score of noteScores) {
+                if (score === 115) count++;
+                else count = 0;
+                maxCount = maxCount < count ? count : maxCount;
+            }
+            result.push([obj.songName, maxCount]);
+        } catch {}
+    });
+});
+
+result.sort((a, b) => a[1] - b[1]);
+for (var res of result) {
+    if (res[1] > 10) console.log(res[0], res[1]);
+}
